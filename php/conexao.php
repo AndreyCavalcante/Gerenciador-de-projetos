a86216d8de04ee6bdd7d28852bd2e6c17d6ab189
Andrey Lopes Cavalcante Mendes <129072446+AndreyCavalcante@users.noreply.github.com>
2024-08-24 23:51:03 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,12 +1,12 @@
 <?php
 
-    $local = "localhost";
-    $user = "root";
-    $senha = "";
-    $banco = "gerenciador_de_projetos";
+    $local = "sql10.freemysqlhosting.net";
+    $user = "sql10727706";
+    $senha = "RE5uEVYWdE";
+    $banco = "sql10727706";
 
     $conecta = new mysqli($local, $user, $senha, $banco);
 
     if($conecta->connect_error === true){
-        die("Deu ruim malandro!");
+        die("Conexão não concluida");
     }
